d790add980f047be95b6bc0a11d40965a4ef4c45
Киль Александр <kooler.kilev@gmail.com>
2019-11-06 22:56:40 +0700

    Исправил замечания

--- task1/src/main/java/ru/cft/focusstart/Main.java
+++ task1/src/main/java/ru/cft/focusstart/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         int size = UserQuestioner.askUserForTableSize();
-        ConsolePrinter.print(new NxNTableBuilder(MultiplicationTableUtils.generateTable(size)
-                , MultiplicationTableUtils.getMaxValue(size)).build());
+        TableBuilder tableBuilder = new TableBuilder(MultiplicationTableUtils.generateTable(size),
+                MultiplicationTableUtils.getMaxValue(size));
+        ConsolePrinter.print(tableBuilder.build());
     }
 }
--- task1/src/main/java/ru/cft/focusstart/TableBuilder.java
+++ task1/src/main/java/ru/cft/focusstart/TableBuilder.java
@@ -1,12 +1,12 @@
 package ru.cft.focusstart;
 
-class NxNTableBuilder {
+class TableBuilder {
 
     private static final char VERTICAL_SEPARATOR = '|';
     private static final char HORIZONTAL_SEPARATOR = '-';
     private static final char NODE_SEPARATOR = '+';
     private static final char SPACE = ' ';
-    private static final char NEWLINE = '\n';
+    private static final String NEWLINE = System.lineSeparator();
 
     private final int[][] table;
     private final int tableSize;
@@ -14,7 +14,7 @@ class NxNTableBuilder {
     private final int leftCellSize;
     private final String separatorLine;
 
-    NxNTableBuilder(int[][] table, int maxValue) {
+    TableBuilder(int[][] table, int maxValue) {
         this.table = table;
         this.tableSize = table.length;
         cellSize = String.valueOf(maxValue).length();
@@ -23,9 +23,9 @@ class NxNTableBuilder {
     }
 
     String build() {
-        StringBuilder stringBuilder = new StringBuilder()
-                .append(getTopHeader());
+        StringBuilder stringBuilder = new StringBuilder();
         stringBuilder
+                .append(getTopHeader())
                 .delete(stringBuilder.length() - 1, stringBuilder.length())
                 .append(NEWLINE)
                 .append(separatorLine).append(NEWLINE);
@@ -40,14 +40,15 @@ class NxNTableBuilder {
                     .append(separatorLine)
                     .append(NEWLINE);
         }
-        return String.valueOf(stringBuilder);
+        return stringBuilder.toString();
     }
 
     private String getCell(int value, int cellSize) {
-        return String.valueOf(new StringBuilder()
+        return new StringBuilder()
                 .append(getCellSpace(value, cellSize))
                 .append(value)
-                .append(VERTICAL_SEPARATOR));
+                .append(VERTICAL_SEPARATOR)
+                .toString();
     }
 
     private String getTopHeader() {
@@ -58,7 +59,7 @@ class NxNTableBuilder {
         for (int i = 0; i < tableSize; i++) {
             stringBuilder.append(getCell(i + 1, cellSize));
         }
-        return String.valueOf(stringBuilder);
+        return stringBuilder.toString();
     }
 
     private String getCellSpace(int value, int cellSize) {
@@ -66,7 +67,7 @@ class NxNTableBuilder {
         for (int i = 0; i < cellSize - String.valueOf(value).length(); i++) {
             stringBuilder.append(SPACE);
         }
-        return String.valueOf(stringBuilder);
+        return stringBuilder.toString();
     }
 
     private String generateSeparatorLine() {
