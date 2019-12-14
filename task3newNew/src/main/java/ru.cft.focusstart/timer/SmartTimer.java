e9528b0bac12edbe2b4f3018dc4098d0dfd659f7
Киль Александр <kooler.kilev@gmail.com>
2019-12-14 21:13:32 +0700

    переделал таймер

--- task3newNew/src/main/java/ru.cft.focusstart/timer/SmartTimer.java
+++ task3newNew/src/main/java/ru.cft.focusstart/timer/SmartTimer.java
@@ -5,54 +5,72 @@ import ru.cft.focusstart.dto.TimeChangeDto;
 import ru.cft.focusstart.observer.IObserverManager;
 import ru.cft.focusstart.observer.Observer;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class SmartTimer implements ITimer, Observer<GameStateChangeDto> {
 
     private final IObserverManager observerManager;
-    private Timer timer;
 
-    private long systemNanoTime;
+    private ScheduledExecutorService service;
     private AtomicInteger time = new AtomicInteger(0);
+    private AtomicBoolean firstTick = new AtomicBoolean(true);
 
     public SmartTimer(IObserverManager observerManager) {
         this.observerManager = observerManager;
         observerManager.addObserver(GameStateChangeDto.class, this);
     }
 
-    class timeTask extends TimerTask implements Runnable {
+    class Tick implements Runnable {
         @Override
         public void run() {
-            if (System.nanoTime() - 1000000000 > systemNanoTime) {
-                systemNanoTime = System.nanoTime();
-                observerManager.notifyObservers(new TimeChangeDto(time.incrementAndGet()));
+            if (firstTick.get()) {
+                firstTick.set(false);
+            } else {
+                time.getAndIncrement();
             }
+            sendNotify();
         }
-    }
-
-    @Override
-    public Integer getTime() {
-        return time.get();
     }
 
     @Override
     public void handleEvent(GameStateChangeDto dto) {
         switch (dto.getGameState()) {
             case RUN:
-                reset();
+                continueTime();
                 return;
+            case READY_TO_RUN:
+                resetTime();
             case LOSE:
             case WIN:
-                timer.cancel();
+                pause();
         }
     }
 
-    private void reset() {
+    private void continueTime() {
+        service = Executors.newSingleThreadScheduledExecutor();
+        service.scheduleAtFixedRate(new Tick(), 0, 1, TimeUnit.SECONDS);
+    }
+
+    private void resetTime() {
         time.set(0);
-        systemNanoTime = System.nanoTime();
-        timer = new Timer();
-        timer.schedule(new timeTask(), 0, 500);
+        firstTick.set(true);
+        sendNotify();
+    }
+
+    private void pause() {
+        service.shutdown();
+    }
+
+    private void sendNotify() {
+        observerManager.notifyObservers(new TimeChangeDto(time.get()));
+    }
+
+    @Override
+    public Integer getTime() {
+        return time.get();
     }
 }
